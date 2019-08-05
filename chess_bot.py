83d47d94203066ea83f6fdb33d6bb5e528c072db
Borey-PC <buttscl@mail.uc.edu>
2019-08-05 00:16:20 -0400

    Fixed minimax and implemented alpha beta pruning

--- chess_bot.py
+++ chess_bot.py
@@ -2,6 +2,8 @@ import chess
 import chess.svg
 import random
 
+INF = 999999
+
 def print_board(board):
     board = str(board)
     print()
@@ -140,61 +142,59 @@ def evaluate_position(board):
         elif char == ".":
             i += 1
 
+    if board.is_check() and board.turn:
+        score -= 900
+    elif board.is_check() and not board.turn:
+        score += 900
+
     return score
 
-def traverse_tree(board, white, depth):
-    best_score = 0
-    legal_moves = list(board.legal_moves)
-    count = 0
+def minimax(board, depth, height, white, alpha, beta):
+    if depth == 0:
+        return evaluate_position(board)
 
-    for m in legal_moves:
-        count += 1
-        move_piece(board, str(m))
-        if depth == 0:
-            score = evaluate_position(board)
-            if white and score > best_score:
-                best_score = score
-            elif not white and score < best_score:
-                best_score = score
+    legal_moves = list(board.legal_moves)    
+    
+    if white:
+        if not(len(legal_moves) > 0):
+            return -INF + 1
+        best_score = -INF + 1
+        best_move = str(random.choice(legal_moves))
+        for m in legal_moves:
+            move_piece(board, str(m))
+            score = minimax(board, depth-1, height, False, alpha, beta)
             board.pop()
+            if score > best_score:
+                best_score = score
+                best_move = str(m)
+            alpha = max(alpha,best_score)
+            if beta <= alpha:
+                break
+        if depth == height:
+            return best_move
         else:
-            best_score = traverse_tree(board, white, depth-1)
+            return best_score
+
+    else:
+        if not(len(legal_moves) > 0):
+            return INF - 1
+        best_score = INF - 1
+        best_move = str(random.choice(legal_moves))
+        for m in legal_moves:
+            move_piece(board, str(m))
+            score = minimax(board, depth-1, height, True, alpha, beta)
             board.pop()
-
-    return best_score
-
-def best_move(board, white, depth):
-    legal_moves = list(board.legal_moves)
-    best_score = 0
-    last_score = evaluate_position(board)
-    move = str(legal_moves[0])
-    count = 0
-
-
-    for m in legal_moves:
-        count += 1
-        move_piece(board, str(m))
-        if depth == 0:
-            score = evaluate_position(board)
+            if score < best_score:
+                best_score = score
+                best_move = str(m)
+            alpha = max(alpha, best_score)
+            if beta <= alpha:
+                break
+        if depth == height:
+            return best_move
         else:
-            score = traverse_tree(board, white, depth-1)
-
-        if white and score > best_score:
-            best_score = score
-            move = str(board.peek())
-        elif not white and score < best_score:
-            best_score = score
-            move = str(board.peek())
-        board.pop()
+            return best_score
         
-
-    if best_score == last_score:
-        print("Random Move")
-        move = str(random.choice(legal_moves))
-    
-    print(move)
-    
-    return move
 
 board = chess.Board()
 white_score = 0
@@ -203,17 +203,22 @@ print("----------------------------------------------------")
 print("Score: " + str(white_score))
 print_board(board)
 
+W_DEPTH = 2
+B_DEPTH = 3
+
 while board.is_checkmate:
     # User plays white
     """ while board.turn:
         white_move = input("Your Move: ")
-        try:
-            move_piece(board, white_move)
-        except:
-            print("Illegal Move! Try again") """
+        for m in board.legal_moves:
+            if white_move == str(m):
+                move_piece(board, white_move)
+                break
+        if board.turn:
+            print("Illegal move!") """
     
     print("White Move")
-    white_move = best_move(board, True, 3)
+    white_move = minimax(board, W_DEPTH, W_DEPTH, True, -INF, INF)
     move_piece(board, white_move)
 
     white_score = evaluate_position(board)
@@ -221,11 +226,15 @@ while board.is_checkmate:
     print("----------------------------------------------------")
     print("Score: " + str(white_score))
     print_board(board)
-    
+    print("----------------------------------------------------")
+
+    if board.is_checkmate():
+        print("CHECKMATE! WHITE WINS!")
+        exit()
     #input()
 
     print("Black Move")
-    black_move = best_move(board, False, 3)
+    black_move = minimax(board, B_DEPTH, B_DEPTH, False, -INF, INF)
     move_piece(board, black_move)
     
     white_score = evaluate_position(board)
@@ -233,5 +242,9 @@ while board.is_checkmate:
     print("----------------------------------------------------")
     print("Score: " + str(white_score))
     print_board(board)
+    print("----------------------------------------------------")
 
+    if board.is_checkmate():
+        print("CHECKMATE! BLACK WINS!")
+        exit()
     #input()
